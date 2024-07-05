d5044d9f7b84e56db97f62d16688f27384bfa7bb
thiago <thiago@lepidus.com.br>
2024-07-05 18:45:23 -0400

    Create ThothException class
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#847
    
    Signed-off-by: thiago <thiago@lepidus.com.br>

---
+++ thoth/exceptions/ThothException.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file plugins/generic/thoth/thoth/exceptions/ThothException.inc.php
+ *
+ * Copyright (c) 2024 Lepidus Tecnologia
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @brief Exception indicating a connection error with Thoth API.
+ */
+
+class ThothException extends \RuntimeException
+{
+    public function __construct($error, $code)
+    {
+        parent::__construct('Failed to send the request to Thoth: ' . $error, $code);
+    }
+}
