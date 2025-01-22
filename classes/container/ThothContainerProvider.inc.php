25b1f14ff439b94b90855f4f276af34d0185c73e
Thiago Brasil <thiago@lepidus.com.br>
2025-01-22 16:17:33 -0400

    Configures thoth client container binding
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#929
    
    Signed-off-by: Thiago Brasil <thiago@lepidus.com.br>

--- classes/container/ThothContainerProvider.inc.php
+++ classes/container/ThothContainerProvider.inc.php
@@ -13,14 +13,50 @@
  * @brief Utility class to package all plugin container bindings
  */
 
+use ThothApi\GraphQL\Client;
+
 import('plugins.generic.thoth.classes.container.ContainerProvider');
+import('plugins.generic.thoth.classes.services.ThothAffiliationService');
+import('plugins.generic.thoth.classes.services.ThothContributionService');
+import('plugins.generic.thoth.classes.services.ThothContributorService');
+import('plugins.generic.thoth.classes.services.ThothInstitutionService');
+import('plugins.generic.thoth.classes.services.ThothLanguageService');
+import('plugins.generic.thoth.classes.services.ThothLocationService');
+import('plugins.generic.thoth.classes.services.ThothPublicationService');
+import('plugins.generic.thoth.classes.services.ThothReferenceService');
+import('plugins.generic.thoth.classes.services.ThothSubjectService');
+import('plugins.generic.thoth.classes.services.ThothWorkService');
+import('plugins.generic.thoth.lib.APIKeyEncryption.APIKeyEncryption');
 
 class ThothContainerProvider implements ContainerProvider
 {
     public function register($container)
     {
+        $container->set('config', function ($container) {
+            $pluginSettingsDao = & DAORegistry::getDAO('PluginSettingsDAO');
+            $contextId = Application::get()->getRequest()->getContext()->getId();
+
+            $testEnvironment = $pluginSettingsDao->getSetting($contextId, 'ThothPlugin', 'testEnvironment');
+            $email = $pluginSettingsDao->getSetting($contextId, 'ThothPlugin', 'email');
+            $password = $pluginSettingsDao->getSetting($contextId, 'ThothPlugin', 'password');
+
+            return [
+                'testEnvironment' => $testEnvironment,
+                'email' => $email,
+                'password' => APIKeyEncryption::decryptString($password)
+            ];
+        });
+
         $container->set('client', function ($container) {
-            return new Client();
+            $config = $container->get('config');
+
+            $httpConfig = [];
+            if ($config['testEnvironment']) {
+                $httpConfig['base_uri'] = 'http://localhost:8000/';
+            }
+
+            $client = new Client($httpConfig);
+            return $client->login($config['email'], $config['password']);
         });
 
         $container->set('affiliationService', function ($container) {
