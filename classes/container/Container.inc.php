96838842c9502b0208719fe957e6bc62e474e16e
Thiago Brasil <thiago@lepidus.com.br>
2025-01-23 16:51:13 -0400

    Implements a simple dependency injection container
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#929
    
    Signed-off-by: Thiago Brasil <thiago@lepidus.com.br>

---
+++ classes/container/Container.inc.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file plugins/generic/thoth/tests/classes/container/Container.inc.php
+ *
+ * Copyright (c) 2025 Lepidus Tecnologia
+ * Copyright (c) 2025 Thoth
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class Container
+ * @ingroup plugins_generic_thoth
+ *
+ * @brief Simple dependency injection container implementation
+ */
+
+class Container
+{
+    private $bindings = [];
+
+    public function set($id, $factory)
+    {
+        $this->bindings[$id] = $factory;
+    }
+
+    public function get($id)
+    {
+        if (!isset($this->bindings[$id])) {
+            throw new Exception("Target binding \"$id\" does not exist.");
+        }
+
+        $factory = $this->bindings[$id];
+
+        return $factory($this);
+    }
+
+    public function backup($id)
+    {
+        return $this->bindings[$id];
+    }
+
+    public function register($containerProvider)
+    {
+        $containerProvider->register($this);
+    }
+}
---
+++ tests/classes/container/ThothContainerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @file plugins/generic/thoth/tests/classes/container/ThothContainerTest.php
+ *
+ * Copyright (c) 2025 Lepidus Tecnologia
+ * Copyright (c) 2025 Thoth
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class ThothContainerTest
+ * @ingroup plugins_generic_thoth_tests
+ * @see ThothContainer
+ *
+ * @brief Test class for the ThothContainer class
+ */
+
+import('lib.pkp.tests.PKPTestCase');
+import('plugins.generic.thoth.classes.container.ThothContainer');
+
+class ThothContainerTest extends PKPTestCase
+{
+    public function testGetSameContainerInstance()
+    {
+        $firstContainer = ThothContainer::getInstance();
+        $secondContainer = ThothContainer::getInstance();
+
+        $this->assertInstanceOf(ThothContainer::class, $firstContainer);
+        $this->assertSame($firstContainer, $secondContainer);
+    }
+
+    public function testReplaceContainerBinding()
+    {
+        ThothContainer::getInstance()->set('foo', function () {
+            return 'foo';
+        });
+
+        $fooFoo = ThothContainer::getInstance()->get('foo');
+
+        ThothContainer::getInstance()->set('foo', function () {
+            return 'bar';
+        });
+
+        $fooBar = ThothContainer::getInstance()->get('foo');
+
+        $this->assertEquals('foo', $fooFoo);
+        $this->assertEquals('bar', $fooBar);
+    }
+}
