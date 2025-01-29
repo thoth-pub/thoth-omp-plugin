6695a959668295a1e249206647057209d5814bd3
Thiago Matos <thiago@lepidus.com.br>
2025-01-29 19:14:43 -0400

    Created institution repository class
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#932
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>

---
+++ classes/repositories/ThothInstitutionRepository.inc.php
@@ -0,0 +1,38 @@
+<?php
+
+use ThothApi\GraphQL\Models\Institution as ThothInstitution;
+
+class ThothInstitutionRepository
+{
+    protected $thothClient;
+
+    public function __construct($thothClient)
+    {
+        $this->thothClient = $thothClient;
+    }
+
+    public function new(array $data = [])
+    {
+        return new ThothInstitution($data);
+    }
+
+    public function get($thothInstitutionId)
+    {
+        return $this->thothClient->Institution($thothInstitutionId);
+    }
+
+    public function add($thothInstitution)
+    {
+        return $this->thothClient->createInstitution($thothInstitution);
+    }
+
+    public function edit($thothPatchInstitution)
+    {
+        return $this->thothClient->updateInstitution($thothPatchInstitution);
+    }
+
+    public function delete($thothInstitutionId)
+    {
+        return $this->thothClient->deleteInstitution($thothInstitutionId);
+    }
+}
