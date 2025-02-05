824aba28ee6f649bcca5b5ab3387ea5a5eb4dbba
Thiago Matos <thiago@lepidus.com.br>
2025-02-05 17:57:13 -0400

    Created class to remove HTML tags from strings
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#932
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>

---
+++ tests/classes/formatters/HtmlStripperTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file plugins/generic/thoth/tests/classes/formatters/HtmlStripperTest.php
+ *
+ * Copyright (c) 2025 Lepidus Tecnologia
+ * Copyright (c) 2025 Thoth
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class HtmlStripperTest
+ * @ingroup plugins_generic_thoth_tests
+ * @see HtmlStripper
+ *
+ * @brief Test class for the HtmlStripper class
+ */
+
+import('lib.pkp.tests.PKPTestCase');
+import('plugins.generic.thoth.classes.formatters.HtmlStripper');
+
+class HtmlStripperTest extends PKPTestCase
+{
+    public function testStripHtmlTags()
+    {
+        $string = '<p>This is a <a href="#">string</a> with <strong>html tags</strong></p>';
+
+        $strippedString = HtmlStripper::stripTags($string);
+
+        $this->assertSame('<p>This is a string with <strong>html tags</strong></p>', $strippedString);
+    }
+}
