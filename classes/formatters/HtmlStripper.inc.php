39b5eff20dc918506d240ae5076b35d7788df487
Thiago Matos <thiago@lepidus.com.br>
2025-02-17 14:40:48 -0400

    Added HtmlStripper class
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#932
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>

---
+++ classes/formatters/HtmlStripper.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file plugins/generic/thoth/classes/formatters/HtmlStripper.php
+ *
+ * Copyright (c) 2024-2025 Lepidus Tecnologia
+ * Copyright (c) 2024-2025 Thoth
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class HtmlStripper
+ *
+ * @ingroup plugins_generic_thoth
+ *
+ * @brief Helper class for removing HTML tags from strings
+ */
+
+class HtmlStripper
+{
+    private const ALLOWED_TAGS = '<b><strong><em><i><u><ul><ol><li><p><h1><h2><h3><h4><h5><h6>';
+
+    public static function stripTags($string)
+    {
+        return strip_tags($string, self::ALLOWED_TAGS);
+    }
+}
