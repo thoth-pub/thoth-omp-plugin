2fb7da4b4b8285e908b3a0a7d6473c89f779a6d7
Thiago Matos <thiago@lepidus.com.br>
2026-07-09 13:33:36 -0400

    feat: add frontcover option to catalog entry
    Refs documentacao-e-tarefas/desenvolvimento_e_infra#1214

--- classes/components/forms/config/CatalogEntryFormConfig.inc.php
+++ classes/components/forms/config/CatalogEntryFormConfig.inc.php
@@ -13,6 +13,9 @@
  * @brief Thoth config for catalog entry form
  */
 
+import('plugins.generic.thoth.classes.facades.ThothRepo');
+import('plugins.generic.thoth.classes.services.ThothMeCacheService');
+
 class CatalogEntryFormConfig
 {
     public function addConfig($hookName, $form)
@@ -23,21 +26,55 @@ class CatalogEntryFormConfig
 
         $actionParts = explode('/', $form->action);
         $publicationId = end($actionParts);
-        $publication = Services::get('publication')->get($publicationId);
+        $publication = $this->getPublication($publicationId);
 
         $form->addField(new \PKP\components\forms\FieldText('place', [
             'label' => __('plugins.generic.thoth.field.place.label'),
             'value' => $publication->getData('place'),
         ]))
-        ->addField(new \PKP\components\forms\FieldText('pageCount', [
-            'label' => __('plugins.generic.thoth.field.pageCount.label'),
-            'value' => $publication->getData('pageCount'),
-        ]))
-        ->addField(new \PKP\components\forms\FieldText('imageCount', [
-            'label' => __('plugins.generic.thoth.field.imageCount.label'),
-            'value' => $publication->getData('imageCount'),
-        ]));
+            ->addField(new \PKP\components\forms\FieldText('pageCount', [
+                'label' => __('plugins.generic.thoth.field.pageCount.label'),
+                'value' => $publication->getData('pageCount'),
+            ]))
+            ->addField(new \PKP\components\forms\FieldText('imageCount', [
+                'label' => __('plugins.generic.thoth.field.imageCount.label'),
+                'value' => $publication->getData('imageCount'),
+            ]));
+
+        $canUploadFiles = $this->canUploadFiles($publication);
+        $form->addField(new \PKP\components\forms\FieldOptions('thothUploadFrontcover', [
+            'label' => __('plugins.generic.thoth.field.frontcover'),
+            'description' => $canUploadFiles
+                ? null
+                : __('plugins.generic.thoth.field.frontcover.missingCdnWritePermission'),
+            'options' => [
+                [
+                    'value' => true,
+                    'label' => __('plugins.generic.thoth.field.frontcover.label'),
+                    'disabled' => !$canUploadFiles,
+                ],
+            ],
+            'value' => $publication->getData('thothUploadFrontcover') && $canUploadFiles ? [true] : [],
+        ]), ['after', 'coverImage']);
 
         return false;
+    }
+
+    protected function getPublication($publicationId)
+    {
+        return Services::get('publication')->get($publicationId);
+    }
+
+    protected function canUploadFiles($publication): bool
+    {
+        try {
+            $submission = Services::get('submission')->get($publication->getData('submissionId'));
+            return (new ThothMeCacheService(ThothRepo::me()))->hasCdnWritePermission(
+                $submission->getData('contextId')
+            );
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+            return false;
+        }
     }
 }
