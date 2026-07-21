362061d66244f2a5ed178e5f86e828ba1e7fcddc
Thiago Matos <thiago@lepidus.com.br>
2026-07-21 19:05:26 -0400

    fix: match renamed contributions by ordinal
    refs documentacao-e-tarefas/desenvolvimento_e_infra#1222

--- classes/services/ThothContributionService.inc.php
+++ classes/services/ThothContributionService.inc.php
@@ -118,6 +118,7 @@ class ThothContributionService
             $existingKey = $this->findMatchingContributionKey(
                 $author,
                 $thothContribution->getContributionType(),
+                $thothContribution->getContributionOrdinal(),
                 $remainingContributions
             );
             if ($existingKey === null) {
@@ -182,12 +183,22 @@ class ThothContributionService
     private function findMatchingContributionKey(
         $author,
         string $contributionType,
+        int $contributionOrdinal,
         array $existingContributions
     ): ?int {
         foreach ($existingContributions as $key => $existingContribution) {
             if (
                 ($existingContribution['contributionType'] ?? null) === $contributionType
                 && $this->isSameAuthor($author, $existingContribution)
+            ) {
+                return $key;
+            }
+        }
+
+        foreach ($existingContributions as $key => $existingContribution) {
+            if (
+                ($existingContribution['contributionType'] ?? null) === $contributionType
+                && ($existingContribution['contributionOrdinal'] ?? null) === $contributionOrdinal
             ) {
                 return $key;
             }
